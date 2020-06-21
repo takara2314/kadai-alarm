7d09ee9c7721fc0b30b1d18adc98cd98f240c8b4
takara2314 <takara2314_TK@outlook.jp>
2020-06-21 21:36:57 +0900

    Go側でタイムゾーンをJSTにした

--- init.go
+++ init.go
@@ -4,9 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
+
+const location = "Asia/Tokyo"
 
 var (
 	dbDriver  string = os.Getenv("DRIVER_NAME")
@@ -17,6 +20,14 @@ var (
 
 func init() {
 	var err error
+
+	// GAEはタイムゾーン指定できないので、Go側でタイムゾーンを指定する
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+	time.Local = loc
+
 	db, err = sql.Open(dbDriver, dbURL)
 	if err != nil {
 		panic(err)
