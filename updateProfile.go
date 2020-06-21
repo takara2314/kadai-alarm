027ffeb6f0a98c7ce715814a13f1ae221a22a604
takara2314 <takara2314_TK@outlook.jp>
2020-06-21 17:36:23 +0900

    HerokuからGAE用のコードに移行3

---
+++ updateProfile.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+)
+
+func updateProfile(subject string, toNormal bool) {
+	api := getTwitterAPI()
+
+	var postUserName string
+	if !toNormal {
+		var userName string = os.Getenv("USER_NAME")
+		postUserName = userName + "@" + subject + "の課題未提出かもよ"
+	} else {
+		postUserName = os.Getenv("USER_NAME")
+	}
+
+	changeThings := url.Values{}
+	changeThings.Set("name", postUserName)
+	api.PostAccountUpdateProfile(changeThings)
+	fmt.Println("ツイッターの名前を変更しました…")
+}
+
+func getTwitterAPI() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
+
+	return anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+}
