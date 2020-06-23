23ddd490580859d68874dc1562c38e2cd59506ac
takara2314 <takara2314_TK@outlook.jp>
2020-06-23 22:18:35 +0900

    RDB(Postgres)からNoSQL(Firestore)用のコードに書き換えている途中

--- init.go
+++ init.go
@@ -1,45 +1,52 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"os"
+	"log"
 	"time"
 
-	_ "github.com/lib/pq"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
+// タイムゾーンを設定
 const location = "Asia/Tokyo"
 
 var (
-	dbDriver       string = os.Getenv("DRIVER_NAME")
-	dbURL          string = os.Getenv("DATABASE_URL")
-	tableName      string = "homeworks"
-	db             *sql.DB
 	oldTwitterName string = "ふぉくしーど"
+	ctx            context.Context
+	app            *firebase.App
+	collectionName string = "homeworks"
+	alarmTimes            = make(map[string][]interface{})
 )
 
 func init() {
 	var err error
 
-	// GAEはタイムゾーン指定できないので、Go側でタイムゾーンを指定する
+	// Google App Engine はタイムゾーン指定できないので、Go側でタイムゾーンを指定する
 	loc, err := time.LoadLocation(location)
 	if err != nil {
 		loc = time.FixedZone(location, 9*60*60)
 	}
 	time.Local = loc
 
-	db, err = sql.Open(dbDriver, dbURL)
+	// 利用するデータベースを宣言
+	ctx = context.Background()
+	sa := option.WithCredentialsFile("kadai-alarm-5365dc12423d.json")
+
+	// データベースを開くこのアプリを初期化
+	app, err = firebase.NewApp(ctx, nil, sa)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	} else {
 		fmt.Println("データベースに接続成功しました。")
 	}
 
-	// 久しぶりの起動を予期して、最初に初期化してフレッシュな予定を取得する
+	// 現在のデータベースのhomeworksコレクションを全初期化
 	dbDelete()
+	// TimeTreeのスケジュールを取得
 	getSchedule()
 
-	// 現在のアラーム一覧
-	nowAlarms()
+	fmt.Println("起動完了しました。指定された時間になりましたら処理が開始されます。")
 }
